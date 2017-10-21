6e76d902b37a292c16e79bb6bb77d4a7d3312d43
Belma Jakupovic <belma.jakupovic@ag04.com>
2017-10-21 19:21:26 +0200

    Initial commit

---
+++ src/main/java/com/bookme/BookMe/BookMeApplication.java
@@ -0,0 +1,12 @@
+package com.bookme.BookMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMeApplication.class, args);
+	}
+}
